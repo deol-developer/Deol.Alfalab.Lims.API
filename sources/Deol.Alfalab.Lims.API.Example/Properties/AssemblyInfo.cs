bd1e47ab25c41cb11c78e650ef3e1c583899eab5
Олег <devjatkin.oleg@rambler.ru>
2021-02-11 13:29:38 +0300

    Configured nuget package v1.1.0

--- sources/Deol.Alfalab.Lims.API.Example/Properties/AssemblyInfo.cs
+++ sources/Deol.Alfalab.Lims.API.Example/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Deol.Alfalab.Lims.API.Example")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright ©  2020, Oleg Devyatkin")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
